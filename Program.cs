76259228016b48b4f8b9fa5c9eec3de95dc26152
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-10-23 18:40:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] array2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] commonElements = array1.Intersect(array2).ToArray();
+
+        Console.WriteLine(string.Join(" ", commonElements));
+    }
+}
